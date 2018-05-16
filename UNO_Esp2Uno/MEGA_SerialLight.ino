ec5d15b6cc30ccd517ee712c1b3855c8716ad890
Geekoid85 <38323963+Geekoid85@users.noreply.github.com>
2018-05-16 17:51:09 +0200

    Update and rename UNO_Esp2Uno.ino to MEGA_SerialLight.ino
    This sketch turn on or off the pin 13 led using two Serial command:
    - on
    - off

--- UNO_Esp2Uno/MEGA_SerialLight.ino
+++ UNO_Esp2Uno/MEGA_SerialLight.ino
@@ -1,15 +1,15 @@
 /*---------- Informations on the project ----------*/
-// This sketch is for testing the serial communication between the esp32 and an arduino uno.
+// This sketch is for testing the serial communication between the esp32 and an arduino mega.
 // On another sketch (ESP32_Udp2Serial) the esp is receiving data via a wifi UDP access point and sending it over Serial to the arduino (this following sketch)
 // This will be a part of the School_InformativeScreen project GPL licence
 // https://github.com/Geekoid85/School_InformativeScreen
 
 /*---------- Informations about wiring ----------*/
-// Just connect the tx pin of the esp32 to the rx pin of the arduino UNO (resistor is not needed in this way)
+// Just connect the tx pin of the esp32 to the rx pin of the arduino mega (resistor is not needed in this way)
 // And the ground, that's it !
 
 
-int led(13); // This is the pin of the onBoard led of the arduino uno
+int led(13); // This is the pin of the onBoard led of the arduino mega
 void setup () {
   pinMode(led,OUTPUT); // led's pin is an OUTPUT
   digitalWrite(led,LOW); // make sure it's turn off
